3da8d67d7ce93d620a64ac404afbe23cdcc90d69
Milica Jovanovic <j.milica95@yahoo.com>
2016-06-07 22:18:34 +0200

    Initial commit

---
+++ javaScript.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.title = "Met Hotels";
+    $scope.about = "O hotelu";
+    $scope.reservation = "Rezervacija smeštaja";
+    $scope.registration = "Registracija";
+    $scope.login = "Log in";
+    $scope.contentTitle = "Dobro došli u 'Met Hotels'!";
+    $scope.aboutTitle = "O hotelu";
+    $scope.reservationTitle = "Rezervacija smeštaja";
+    $scope.registrationTitle = "Registracija";
+    $scope.addRoom = "Dodavanje sobe";
+    $scope.addRoomTitle = "Dodavanje novih soba u hotel";
+
+    $scope.reserve = function() {
+    	alert("Uspešno ste rezervisali smeštaj!");
+    };
+
+    $(document).ready(function(){
+        $('#table').DataTable();
+    });
+
+   
+});
